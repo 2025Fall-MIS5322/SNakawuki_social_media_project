d90a243d45d05dfb4ca190d117f4693dd1b66275
Samalie Nakawuki <Sam_Nakawuki1@Baylor.edu>
2025-12-11 20:20:26 -0600

    uploading images

--- FeedProject/settings.py
+++ FeedProject/settings.py
@@ -129,8 +129,12 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/3.2/howto/static-files/
 
 STATIC_URL = '/static/'
-STATIC_ROOT = [BASE_DIR / 'staticfiles'] # Recommended addition for project-level static files
-STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
+STATICFILES_DIRS = [
+    BASE_DIR / 'static',
+]
+STATIC_ROOT = BASE_DIR / 'staticfiles' # Recommended addition for project-level static files
+STATICFILES_STORAGE = "whitenoise.storage.CompressedManifestStaticFilesStorage"
+
 
 
 # Default primary key field type
